0f34de296745e97e575e4b8f5e9d2f20c1efca37
Ram Kowsu <ramanjineyulu.kld@gmail.com>
2023-12-06 17:08:51 +0530

    java+jenkins+docker+k8s

---
+++ src/test/java/io/kowsu/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kowsu.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
