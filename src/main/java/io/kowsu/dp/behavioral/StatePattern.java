4c32c371b5f3ce24655624d445e962fb859b099c
Ram Kowsu <ramanjineyulu.kld@gmail.com>
2024-03-03 13:43:04 +0530

    Adding StatePattern

--- src/main/java/io/kowsu/dp/behavioral/StatePattern.java
+++ src/main/java/io/kowsu/dp/behavioral/StatePattern.java
@@ -1,4 +1,58 @@
 package io.kowsu.dp.behavioral;
 
 public class StatePattern {
+    public static void main(String[] args) {
+        TrafficLight light = new TrafficLight(new RedColorState());
+        light.getCurrentState();
+        light.getCurrentState();
+        light.setLightState(new YellowColorState());
+        light.getCurrentState();
+        light.getCurrentState();
+        light.setLightState(new GreenColorState());
+        light.getCurrentState();
+        light.getCurrentState();
+    }
+}
+
+class TrafficLight {
+
+    private LightState lightState;
+
+    public TrafficLight(LightState lightState) {
+        this.lightState = lightState;
+    }
+
+    public void getCurrentState() {
+        this.lightState.getState();
+    }
+
+    public void setLightState(LightState lightState) {
+        this.lightState = lightState;
+    }
+
+}
+
+interface LightState {
+    void getState();
+}
+
+class RedColorState implements LightState {
+    @Override
+    public void getState() {
+        System.out.println("Current state is red light; vehicles stopped now");
+    }
+}
+
+class GreenColorState implements LightState {
+    @Override
+    public void getState() {
+        System.out.println("Current state is red light; vehicles are running now");
+    }
+}
+
+class YellowColorState implements LightState {
+    @Override
+    public void getState() {
+        System.out.println("Current state is yellow light; vehicles started moving slowly.");
+    }
 }
